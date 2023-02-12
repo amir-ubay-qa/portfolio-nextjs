211f51f93cef29dd7cfafb46e0ecaa3fccab9e95
Amir Ubay <amir.ubaidillah@gmail.com>
2023-02-12 17:23:10 +0700

    Add portfolio repository qa sandbox and portfolio repository

--- src/pages/components/Portfolio.js
+++ src/pages/components/Portfolio.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import cypress from "../assets/cypress.png";
 import robot from "../assets/robot.png";
-import wdio from "../assets/wdio.svg"
+import wdio from "../assets/wdio.svg";
+import next from "../assets/nextjs.png";
 
 const ProjectItem = ({ img, title, desc, link }) => {
   return (
@@ -24,11 +25,13 @@ const ProjectList = () => {
   return (
     <div className="section-container" id="portfolio">
       <h2 className="section-title mt-24 lg:mt-auto"><span className="orange-text">P</span>ortfolio</h2>
-      <p className="section-subtitle">Mockup Automation Test</p>
+      <p className="section-subtitle">My portfolio related with QA</p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-10">
         <ProjectItem img={cypress} title="Cypress" desc="Automation test of e2e and xhr using cypress" link="https://github.com/amir-ubay-qa/cypress" />
         <ProjectItem img={robot} title="Robotframework" desc="Automation test of e2e using robotframework" link="https://github.com/amir-ubay-qa/robotframework" />
         <ProjectItem img={wdio} title="WebdriverIO" desc="Automation test of e2e using webdriverio" link="https://github.com/amir-ubay-qa/webdriver-io" />
+        <ProjectItem img={next} title="NextJS" desc="Sandbox QA for automate testing created with NextJS" link="https://github.com/amir-ubay-qa/sandbox-qa" />
+        <ProjectItem img={next} title="NextJS" desc="This web portfolio's repository created with NextJS" link="https://github.com/amir-ubay-qa/portfolio-nextjs" />
       </div>
     </div>
   );
